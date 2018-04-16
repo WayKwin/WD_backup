df9ec37aeed5efbf1fd39f62d8d640082d98a3d3
WayKwin <543235396@qq.com>
2018-04-16 15:45:27 +0800

    18_4_16 product and consume one by one

--- WD/OS/Thread/p_c_model.c
+++ WD/OS/Thread/p_c_model.c
@@ -45,8 +45,6 @@ void stack_pop(stack*s)
         return;
     if(s->head == NULL )
         return;
-    if(s->head->next == NULL)
-        return;
     node* to_delete = s->head;
     s->head = s->head->next;
     free(to_delete);
@@ -57,8 +55,6 @@ int  stack_isEmpty(stack*s)
     if(s == NULL)
         return 1;
     if(s->head == NULL)
-        return 1;
-    if(s->head->next == NULL)
         return 1;
     return 0;
 }
@@ -84,19 +80,24 @@ void stack_destory(stack* s)
 
 stack demo_stack;
 pthread_mutex_t mutex;
-pthread_cond_t cond;
+pthread_cond_t can_consume;
+pthread_cond_t can_product;
 void* thread_product(void* arg)
 {
     char* who = (char*)arg;
-    int cnt= 100;
-    while(cnt)
+    while(1)
     {
 
-        sleep(3);
+        /*sleep(3);*/
         pthread_mutex_lock(&mutex);
+
         int i = rand()%100 + 1;
         printf("%s product %d\n",who,i);
         stack_push(&demo_stack,i);
+        pthread_cond_signal(&can_consume);
+        /*printf("%s said : send can_consume_signal,then wait can_product signal\n");*/
+        pthread_cond_wait(&can_product, &mutex);
+        /*printf("%s said:  wait can_product signal successfully\n");*/
         pthread_mutex_unlock(&mutex);
     }
             /*(22,31)*/
@@ -110,22 +111,28 @@ void* thread_consume(void* arg)
     while(1)
     {
         pthread_mutex_lock(&mutex);
-        if(!stack_isEmpty(&demo_stack))
+        while(stack_isEmpty(&demo_stack))
         {
+            //cond 必须加锁,如果你刚准备wait(还没wait) 那边恰好发出一个singal信号之后,你刚进入wait状态,那wait就没有意义了
+            //注意如果不释放锁,那么其他线程都会一直等待
+            //因为cond unlock后被唤醒第一件事是恢复加锁
+            printf("%s said: dont have any product,i wait\n",who);
+            pthread_cond_wait(&can_consume,&mutex);
+        }
             i = stack_top(&demo_stack)->data;
             stack_pop(&demo_stack);
             printf("%s comsume %d\n",who,i);
-        }else{
-            pthread_cond_wait(cond,mutex);
-            printf("dont have any product,i wait\n");
-        }
-        pthread_mutex_unlock(&mutex);
+            pthread_cond_signal(&can_product);
+            pthread_mutex_unlock(&mutex);
+
     }
 
 }
 int main()
 {
     pthread_mutex_init(&mutex,NULL);
+    pthread_cond_init(&can_product,NULL);
+    pthread_cond_init(&can_consume,NULL);
     pthread_t producter,consumer;
     pthread_create(&producter, NULL, thread_product, "producter" );
     pthread_create(&consumer, NULL, thread_consume, "consumer");
