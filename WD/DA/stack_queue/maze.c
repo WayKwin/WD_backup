53c0ddceedf11494e79ceb22a2a52a87a0ce5198
WayKwin <543235396@qq.com>
2018-04-17 20:46:18 +0800

    18_4_17 release maze

--- WD/DA/stack_queue/maze.c
+++ WD/DA/stack_queue/maze.c
@@ -1,13 +1,4 @@
- #include<stdio.h>
-#include<unistd.h>
-#include<stdlib.h>
-#define MAX_ROW 6
-#define MAX_COL 6
-
-typedef struct Maze
-{
-    int map[MAX_ROW][MAX_COL] ;
-}Maze;
+#include"maze.h"
 //初始化迷宫
 void MazeInit(Maze* maze)
 {
@@ -50,11 +41,6 @@ void MazePrint(const Maze* maze)
 //Round1 递归方式来解决迷宫问题
 /////////////////////////////////////////
 //
-typedef struct Point
-{
-    int row;
-    int col;
-}Point;
 int  CanStay(Maze* maze, Point cur)
 {
     if(maze == NULL)
@@ -91,62 +77,10 @@ int  IsExit(Maze* maze, Point cur, Point entry )
         return 0;
 
 }
-int   _GETPATH(Maze* maze,Point cur,Point entry)
-{
-    int flag = 0;
-        //就像最长子序列 ,递归无法获得轨迹
-        //因为返回值就一个
-      // n-1 只能获得 n的一个状态(之后会被覆盖)
-        //实际上是把能走1的都走完,是一种遍历
-    if(!CanStay(maze,cur))
-    {
-        return  0;
-    }
-
-    Mark(maze,cur);
-
-    if(IsExit(maze,cur,entry))
-    {
-        maze->map[cur.row][cur.col] = 2;
-
-        printf("找到一条路径\n");
-        return 1;
-    }
-
-     Point p;
-     p.row = cur.row - 1;
-     p.col = cur.col;
-     _GETPATH(maze, p, entry);
-
-     flag = _GETPATH(maze, p, entry);
-
-     p.row = cur.row;
-     p.col = cur.col + 1;
-
-     flag = _GETPATH(maze, p, entry);
-
-     p.row = cur.row + 1;
-     p.col = cur.col ;
-     flag = _GETPATH(maze, p, entry);
-
-     p.row = cur.row;
-     p.col = cur.col - 1;
-     flag = _GETPATH(maze, p, entry);
-
-     return 0;
-
-}
 
 ////////////////////////////////////////
 //Round2 手动记录栈
 ///////////////////////////////////////
-/*#ifdef Round2*/
-#define MAX_POINTSTACK 100
-typedef struct PointStack
-{
-    Point p[MAX_POINTSTACK];
-    int size;
-}PointStack;
 void PointStackInit(PointStack* ps)
 {
     ps->size = 0;
@@ -182,10 +116,32 @@ Point* PointStackTop(PointStack*ps)
     }
     return ps->p+ps->size - 1;
 }
+void PointStackDebugPrint(PointStack * stack, const char*msg)
+{
+    printf("[%s]\n",msg);
+    size_t i = 0;
+    for(; i < stack->size; i++)
+    {
+        printf("(%d, %d)",stack->p[i].row,stack->p[i].col);
+    }
+    printf("\n");
+    //打印顺序表
+}
+void PointStackAssgin(PointStack* stack_dst, PointStack* stack_src)
+{
+    if(stack_src == 0 || stack_dst == 0)
+        return;
+    int i = 0;
+    for(; i < stack_src->size; i++)
+    {
+        stack_dst->p[i] = stack_src->p[i];
+    }
+    stack_dst->size = stack_src->size;
+}
 
-PointStack ps;
 void GetPathByLoop(Maze* maze, Point entry)
 {
+    PointStack ps;
     PointStackInit(&ps);
     Point p = entry;
     PointStackPush(&ps,p);
@@ -193,7 +149,8 @@ void GetPathByLoop(Maze* maze, Point entry)
     {
         if(IsExit(maze,p,entry))
         {
-            break;
+            PointStackDebugPrint(&ps,"路径");
+            PointStackPop(&ps,NULL);
         }
         p.row -= 1;
         PointStackPush(&ps,p);
@@ -255,73 +212,10 @@ void GetPathByLoop(Maze* maze, Point entry)
             else break;
         }
         PointStackPop(&ps,NULL);
-   }
-    //while(canstay ) 遇到exit  循环出栈打印到空,然后继续continue
-    /*while(1)*/
-    /*{*/
-        /*if( (p.row -= 1,  CanStay(maze,p) ))*/
-        /*{*/
-            /*Mark(maze,p);*/
-            /*PointStackPush(&ps,p);*/
-            /*continue;*/
-        /*}*/
-        /*else*/
-        /*{*/
-            /*p.row += 1;*/
-        /*}*/
-        /*if(( p.col += 1 , CanStay(maze,p) ))*/
-        /*{*/
-            /*Mark(maze,p);*/
-            /*PointStackPush(&ps,p);*/
-            /*continue;*/
-        /*}*/
-        /*else*/
-        /*{*/
-            /*p.col -= 1;*/
-        /*}*/
-        /*if((p.row += 1, CanStay(maze,p)))*/
-        /*{*/
-            /*Mark(maze,p);*/
-            /*PointStackPush(&ps,p);*/
-            /*continue;*/
-        /*}*/
-        /*else*/
-        /*{*/
-            /*p.row -= 1;*/
-        /*}*/
-
-
-        /*if(( p.col -= 1 , CanStay(maze,p) ))*/
-        /*{*/
-            /*Mark(maze,p);*/
-            /*PointStackPush(&ps,p);*/
-            /*continue;*/
-         /*}*/
-        /*else*/
-        /*{*/
-            /*p.col += 1;*/
-        /*}*/
-        /*if( PointStackTop(&ps) == NULL)*/
-        /*{*/
-            /*break;*/
-        /*}*/
-        /*if(IsExit(maze,*PointStackTop(&ps),p))*/
-        /*{*/
-            /*break;*/
-        /*}*/
-        /*PointStackPop(&ps,NULL);*/
-     /*}*/
+     }
     MazePrint(maze);
     Point* ret ;
-    printf("一条路径为: ");
-    while(( ret=PointStackTop(&ps) ) != NULL)
-    {
-
-        printf("(%d,%d) ",ret->row,ret->col);
-        PointStackPop(&ps,NULL);
-    }
-    printf("\n");
-
+}
 
     //2 判定入口能否落脚
     //3 标记入口点,并且入口点入栈
@@ -336,18 +230,264 @@ void GetPathByLoop(Maze* maze, Point entry)
     //9如果四个向领的点都不能落脚,就出战当前点,相当于回溯
 
 
+////////////////////////////////////////////////////////////
+//Round3 如果迷宫有多条路径,找到其中的最短路径
+///////////////////////////////////////////////////////////
 
 
+void GETPATH(Maze* maze,Point cur,Point entry, PointStack* cur_path, PointStack* short_path)
+{
+    //1.如果能落脚,对当前点标记,同时把当前点插入 cur_path
+    Point p = cur;
+    if(!CanStay(maze,p))
+    {
+        return;
+    }
+    Mark(maze,p);
+    PointStackPush(cur_path,p);
+    //2.判定当前点是否是出口
+    if(IsExit(maze,p,entry))
+    {
+        printf("找到了一条路径:\n");
+        PointStackDebugPrint(cur_path,"路径");
+        if(short_path->size == 0 || cur_path->size < short_path->size)
+        {
+            printf("找到了目前最短的路径\n");
+            PointStackAssgin(short_path, cur_path);
+        }
+        //到出口就应该回溯,不管是不是目前最小的
+        PointStackPop(cur_path,NULL);
+        return;
+    }
+    //不是出口
+    Point up = cur;
+    up.row -= 1;
+    GETPATH(maze, up, entry, cur_path, short_path);
+    Point right = cur;
+    right.col += 1;
+    GETPATH(maze, right, entry, cur_path, short_path);
+    Point down =cur;
+    down.row += 1;
+    GETPATH(maze, down, entry, cur_path, short_path);
+    Point left =cur;
+    left.col -= 1;
+    GETPATH(maze, left, entry, cur_path, short_path);
+
+    PointStackPop(cur_path, NULL);
+   //3.如果是出口
+    //  size;
+    //  a)和short_path比较,若short_path空或者比short_path短代替完毕也需要回溯
+    //  b)如果当前路径没有比shortpath端,尝试其他路径在回溯之前,要把cur_path栈顶元素出栈(没戏往回退一个)
+    //4. 当前点不是出口,探测四个方向
+    //5.如果四个方向都递归探测过,出栈(没戏往回退)当前栈帧结束,回溯上一点;
+    //就像最长子序列 ,递归无法获得轨迹
+    //因为返回值就一个
+    // n-1 只能获得 n的一个状态(之后会被覆盖)
+    //实际上是把能走1的都走完,是一种遍历
+}
+
+void MazeInitShortPath(Maze* maze)
+{
+     int map[MAX_ROW][MAX_COL] =
+    {
+        { 0, 0, 1, 0, 0, 0 },
+        { 0, 0, 1, 0, 0, 0 },
+        { 1, 1, 1, 1, 1, 1 },
+        { 0, 0, 1, 0, 0, 0 },
+        { 0, 0, 1, 0, 0, 0 },
+        { 0, 0, 1, 0, 0, 0 }
+    };
+    int i;
+    int j;
+    for( i = 0; i < MAX_COL; i++ )
+    {
+        for( j = 0; j < MAX_COL;j++ )
+        {
+            maze->map[i][j] = map[i][j];
+        }
+    }
+
+}
+
+//用栈保存路径
+//找到所有路径,之后筛选出最短路径
+// 两个栈,一个保存当前路径,一个保存最小路径 (选择排序)
+void GetShortPath(Maze* maze,Point entry)
+{
+    //递归版本
+    PointStack cur_path;
+    PointStack short_path;
+    PointStackInit(&cur_path);
+    PointStackInit(&short_path);
+    GETPATH(maze, entry, entry, &cur_path, &short_path);
+    PointStackDebugPrint(&short_path,"最短路径为");
+    //使用递归版本函数 后面两个参数 是两个栈
+    //
+    //打印函数
+}
+/*int main() { [>PointStack cur_path;*/ /*PointStack short_path;*/ /*PointStackInit(&cur_path);*/ /*PointStackInit(&short_path);*/ /*Point p1 ={ 1, 2 };*/ /*Point p2 ={ 3, 4 };*/ /*Point p3 ={ 5, 6 };*/ /*PointStackPush(&cur_path,p1);*/ /*PointStackPush(&cur_path,p2);*/ /*PointStackPush(&cur_path,p3);*/ /*PointStackAssgin(&short_path, &cur_path);*/ /*PointStackDebugPrint(&short_path,"最短路径为");<] Maze maze; Point entry; entry.row = 0; entry.col = 2; MazeInitShortPath(&maze); MazePrint(&maze);*/
+    /*GetShortPath(&maze,entry);*/
+    /*MazePrint(&maze);*/
+    /*return 0;*/
+/*}*/
+////////////////////////////////////////////////////////////
+//Round4 路径多个出口,还带环
+///////////////////////////////////////////////////////////
+// 带环,有可能更小的路径被以前的路径堵住
+// 可以标记走过的步数,k如果步数跟小,就还可以走
+// 递归版本
+void MazeInitShortPathWithCycle(Maze* maze)
+{
+    //当 cur > pre - 1时 pre->cur-
+   //入口点的pre 是1  坐标是 -1,-1
+   /*{ 0, 0, 2, 0, 0, 0 },*/
+   /*{ 0, 0, 3, 0, 0, 0 },*/
+   /*{ 0, 0, 4, 5, 6, 7 },*/
+   /*{ 0, 0, 5, 0, 7, 0 },*/
+   /*{ 9, 8, 6, 7, 8, 0 },*/
+   /*{ 0, 0,11, 0, 0, 0 }*/
+     int map[MAX_ROW][MAX_COL] =
+    {
+        { 0, 0, 1, 0, 0, 0 },
+        { 0, 0, 1, 0, 0, 0 },
+        { 0, 0, 1, 1, 1, 1 },
+        { 0, 0, 1, 0, 1, 0 },
+        { 1, 1, 1, 1, 1, 0 },
+        { 0, 0, 1, 0, 0, 0 }
+    };
+    int i;
+    int j;
+    for( i = 0; i < MAX_COL; i++ )
+    {
+        for( j = 0; j < MAX_COL;j++ )
+        {
+            maze->map[i][j] = map[i][j];
+        }
+    }
+
+}
+int CanStayWithCycle(Maze* maze, Point cur, Point pre)
+{
+    if( maze == NULL )
+        return 0;
+    if( cur.row > MAX_ROW -1 || cur.row > MAX_ROW - 1 || cur.row < 0 || cur.col < 0 )
+        return 0;
+    int cur_value;
+    int pre_value;
+    cur_value = maze->map[cur.row][cur.col];
+    if( cur_value == 0 )
+        return 0;
+    //没走过的直接走
+    if( cur_value == 1 )
+        return 1;
+
+    //走到这就代表cur是以前走到过的点,
+    pre_value = maze->map[pre.row][pre.col];
+    // cur = 7 pre = 6 没必要
+    // cur = 7 pre = 5  可以走,比以前少一步
+    // cur =2  pre = 3  cur是将要走的位置 ,pre是原本的位置
+    if( cur_value >  pre_value + 1 )
+    {
+        return 1;
+    }
+    return 0;
+}
+void MarkWithCycle(Maze* maze, Point cur, Point pre)
+{
+    int cur_value;
+    int pre_value;
+    if(pre.row == -1 && pre.col == -1)
+    {
+        pre_value = 1;
+    }
+    else pre_value = maze->map[pre.row][pre.col];
+    maze->map[cur.row][cur.col] = pre_value + 1;
+}
+void _GetShortPathWithCycle(Maze* maze, Point cur, Point pre, Point entry, PointStack* cur_path, PointStack *short_path)
+{
+    if(!CanStayWithCycle(maze, cur, pre))
+        return;
+    MarkWithCycle(maze, cur, pre);
+    PointStackPush(cur_path, cur);
+    if(IsExit(maze, cur, entry))
+    {
+        PointStackDebugPrint(cur_path, "路径");
+        if( cur_path->size < short_path->size || short_path->size == 0 )
+        {
+            PointStackAssgin(short_path, cur_path);
+        }
+        PointStackPop(cur_path, NULL);
+        return;
+    }
+    MazePrint(maze);
+    sleep(1);
+    Point up = cur;
+    up.row -= 1;
+    _GetShortPathWithCycle(maze, up, cur, entry, cur_path, short_path);
+    Point right = cur;
+    right.col += 1;
+    _GetShortPathWithCycle(maze, right, cur, entry, cur_path, short_path);
+    Point down = cur;
+    down.row += 1;
+    _GetShortPathWithCycle(maze, down, cur, entry, cur_path, short_path);
+    Point left= cur;
+    left.col -= 1;
+    _GetShortPathWithCycle(maze, left, cur, entry, cur_path, short_path);
+    PointStackPop(cur_path, NULL);
+
+}
+void GetShortPathWithCycle(Maze* maze,Point entry)
+{
+    PointStack cur_path;
+    PointStack short_path;
+    PointStackInit(&cur_path);
+    PointStackInit(&short_path);
+    Point pre = {-1, -1};
+    _GetShortPathWithCycle(maze, entry, pre, entry, &cur_path, &short_path);
+    PointStackDebugPrint(&short_path, "最短路径");
 }
 int main()
 {
     Maze maze;
-    Point entry;
-    entry.row = 0;
-    entry.col = 2;
+#ifdef CYCLE
+    MazeInitShortPathWithCycle(&maze);
+    MazePrint(&maze);
+    Point entry  ={ 0, 2 };
+    maze.map[entry.row][entry.col] = 2;
+    GetShortPathWithCycle( &maze, entry);
+#endif
+#ifdef LOOP
     MazeInit(&maze);
-    MazePrint(&maze);
-    GetPathByLoop(&maze,entry);
-    MazePrint(&maze);
-    return 0;
+    Point entry  ={ 0, 2 };
+    GetPathByLoop(&maze, entry);
+#endif
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- WD/OS/Thread/p_c_model.c
+++ WD/OS/Thread/p_c_model.c
@@ -79,7 +79,9 @@ void stack_destory(stack* s)
 
 
 stack demo_stack;
-pthread_mutex_t mutex;
+pthread_mutex_t product_mutex;
+pthread_mutex_t consumer_mutex;
+pthread_mutex_t product_consume_mutex;
 pthread_cond_t can_consume;
 pthread_cond_t can_product;
 void* thread_product(void* arg)
@@ -89,16 +91,18 @@ void* thread_product(void* arg)
     {
 
         /*sleep(3);*/
-        pthread_mutex_lock(&mutex);
-
+        sleep(1);
+        pthread_mutex_lock(&product_mutex);
+        pthread_mutex_lock(&product_consume_mutex);
         int i = rand()%100 + 1;
         printf("%s product %d\n",who,i);
         stack_push(&demo_stack,i);
         pthread_cond_signal(&can_consume);
         /*printf("%s said : send can_consume_signal,then wait can_product signal\n");*/
-        pthread_cond_wait(&can_product, &mutex);
+        pthread_cond_wait(&can_product, &product_consume_mutex);
         /*printf("%s said:  wait can_product signal successfully\n");*/
-        pthread_mutex_unlock(&mutex);
+        pthread_mutex_unlock(&product_consume_mutex);
+        pthread_mutex_unlock(&product_mutex);
     }
             /*(22,31)*/
             /*rand()%8+23*/
@@ -110,34 +114,46 @@ void* thread_consume(void* arg)
     char* who = (char *) arg;
     while(1)
     {
-        pthread_mutex_lock(&mutex);
+        sleep(1);
+        pthread_mutex_lock(&consumer_mutex);
+        pthread_mutex_lock(&product_consume_mutex);
         while(stack_isEmpty(&demo_stack))
         {
             //cond 必须加锁,如果你刚准备wait(还没wait) 那边恰好发出一个singal信号之后,你刚进入wait状态,那wait就没有意义了
             //注意如果不释放锁,那么其他线程都会一直等待
             //因为cond unlock后被唤醒第一件事是恢复加锁
             printf("%s said: dont have any product,i wait\n",who);
-            pthread_cond_wait(&can_consume,&mutex);
+            pthread_cond_wait(&can_consume,&product_consume_mutex);
         }
-            i = stack_top(&demo_stack)->data;
-            stack_pop(&demo_stack);
-            printf("%s comsume %d\n",who,i);
-            pthread_cond_signal(&can_product);
-            pthread_mutex_unlock(&mutex);
-
+        i = stack_top(&demo_stack)->data;
+        stack_pop(&demo_stack);
+        printf("%s comsume %d\n",who,i);
+        pthread_cond_signal(&can_product);
+        pthread_mutex_unlock(&product_consume_mutex);
+        pthread_mutex_unlock(&consumer_mutex);
     }
 
 }
 int main()
 {
-    pthread_mutex_init(&mutex,NULL);
+    pthread_mutex_init(&product_mutex,NULL);
+    pthread_mutex_init(&consumer_mutex,NULL);
+    pthread_mutex_init(&product_consume_mutex,NULL);
     pthread_cond_init(&can_product,NULL);
     pthread_cond_init(&can_consume,NULL);
-    pthread_t producter,consumer;
-    pthread_create(&producter, NULL, thread_product, "producter" );
-    pthread_create(&consumer, NULL, thread_consume, "consumer");
-    pthread_join(producter, NULL);
-    pthread_join(consumer, NULL);
+    pthread_t producter_1, producter_2, producter_3, consumer_1, consumer_2, consumer_3;
+    pthread_create(&producter_1, NULL, thread_product, "producter_1" );
+    pthread_create(&producter_2, NULL, thread_product, "producter_2" );
+    pthread_create(&producter_3, NULL, thread_product, "producter_3" );
+    pthread_create(&consumer_1, NULL, thread_consume, "consumer_1");
+    pthread_create(&consumer_2, NULL, thread_consume, "consumer_2");
+    pthread_create(&consumer_3, NULL, thread_consume, "consumer_3");
+    pthread_join(producter_1, NULL);
+    pthread_join(producter_2, NULL);
+    pthread_join(producter_3, NULL);
+    pthread_join(consumer_1, NULL);
+    pthread_join(consumer_2, NULL);
+    pthread_join(consumer_3, NULL);
 
 }
 /*int main()*/
