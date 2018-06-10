cc625084252d0a096b9a6ada0953988f3e5a87b1
WayKwin <543235396@qq.com>
2018-06-10 21:09:28 +0800

    18.6.10 http

--- OS/Internet/HTTP/service.c
+++ OS/Internet/HTTP/service.c
@@ -1,12 +1,15 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<sys/types.h>
-#include<sys/socket.h>
-#include<arpa/inet.h>
-#include<unistd.h>
-#include<sys/wait.h>
-#define BUFF_SIZE 1024 
-#define PORT 8080
+#include"service.h"
+//注意一定要把文件描述符设置为非阻塞!!!
+void SetNoBlock(int fd)
+{
+  int fdBitMap = fcntl(fd,F_GETFL);
+  if(fdBitMap < 0)
+  {
+    perror("fcntl");
+    return;
+  }
+  fcntl(fd,F_SETFL,fdBitMap|O_NONBLOCK);
+}
 int startup()
 {
   int sock = socket(AF_INET,SOCK_STREAM,0);
@@ -32,21 +35,150 @@ int startup()
   return sock;
 
 }
+int ClearRequest(int sock)
+{
+
+  char buf[BUFF_SIZE];
+  int ret = 0;
+  while(1)
+  {
+    //getline应该是非阻塞版本的
+    ret = get_line(sock,buf,sizeof(buf));
+    if( ret <= 0)
+      break;
+    printf("%s",buf);
+  }
+  return ret;
+}
+int Deal_Header(char* buf,int sock)
+{
+  /*char* method = NULL;*/
+  /*char* RequestPath = NULL;*/
+  /*char* HttpVersion = NULL;*/
+  //错误,这里header[0] 改变了,但是method不会改变, 改变method的指向得它的地址
+  //错误码
+  int ErrorCode = 200;
+  char* header[3] ;
+
+  //获取请求资源信息
+  struct stat file_stat;
+
+  //抽取内容所需
+  int count = 0;
+  int i = 0; 
+  int Space_status = 0;
+
+  for(; buf[i] != '\0'; i++)
+  {
+   if(!isspace(buf[i]) && Space_status == 0) 
+   {
+      header[count++] = buf + i;
+      Space_status = 1;
+   }
+   else if(isspace(buf[i]))
+   {
+     Space_status = 0;
+     buf[i] = '\0';
+   }
+  }
+  char* method = header[0];
+ char* RequestPath = header[1];
+  /*char* HttpVersion = header[2];*/
+#ifdef DEBUG
+  printf("method:%s\n",method);
+  printf("RequestPath:%s\n",RequestPath);
+  printf("HttpVersion:%s\n",HttpVersion);
+#endif
+  //处理method
+  int cgi_flag = 0;
+  char* RequestParameters;
+  //请求应该是忽略大小写的
+  if(strcasecmp(method,"GET") == 0)
+  {
+   for(i = 0; RequestPath[i] != '\0'; i++)
+   {
+     if(RequestPath[i] == '?')
+     {
+       //请求参数在 ? 以后
+       cgi_flag = 1;
+       RequestPath[i++] = '\0';
+       RequestParameters = RequestPath + i;
+       break;
+     }
+   }
+   //从这里结束 是普通cgi
+  }
+  else if(strcasecmp(method,"POST") == 0)
+  {
+    cgi_flag = 1;
+    //请求参数在正文
+    //从正文读取
+  }
+
+  //处理请求资源
+  if(cgi_flag == 1)
+  {
+    //TODO cgi
+    
+  }
+  else 
+  {
+    //记得读完请求
+    ClearRequest(sock);
+    printf("清除结束\n");
+    //发送响应行
+    char* ResponseRow = (char*)"HTTP/1.1 200 ok\n";
+    write(sock,ResponseRow,strlen(ResponseRow));
+  //发送响应头 TODO
+    char* ResponseHeader = (char*)"Server: Waykwin\nContent-Type: text/html;charst=UTF-8\n";
+    write(sock,ResponseHeader,strlen(ResponseHeader));
+    
+    //发送空行
+    write(sock,"\n",1);
+
+    //正文处理
+    sprintf(RequestPath,"./WWW%s",RequestPath);
+    if(strcasecmp(RequestPath,"./WWW/") == 0  )
+    {
+      //打开index.html 发送该文件
+      int index_fd = open("./WWW/index.html",O_RDONLY);
+      if(index_fd < 0)
+      {
+        perror("index_fd error \n");
+        ErrorCode = 404;
+        goto ERROR;
+      }
+      //获取文件大小
+      if(fstat(index_fd,&file_stat) < 0 )
+        perror("fstat:\n");
+
+      if(sendfile(sock,index_fd,0,file_stat.st_size)  < 0)
+        perror("sendfile:");
+    }
+    else 
+    {
+      //指定文件 TODO
+
+    }
+    printf(" 发送完毕\n");
+    
+  }
+ERROR:
+  return ErrorCode;
+  return 1; 
+  //处理http版本
+}
 int get_line(int sock,char buf[],int length)
 {
   char c = 'a';
   int i = 0;
   int ret;
-  while((ret = recv(sock,&c,1,0))> 0 && i < length - 1)
+  while(c != '\n' &&i < length - 1)
   {
-    if(c == '\n')
+    ret = recv(sock,&c,1,0)> 0 ;
+    if( c == '\r' )
     {
-      buf[i++] = c;
-      break;
-    }
-    else if( c == '\r' )
-    {
-      if(recv(sock,&c,1,MSG_PEEK)  > 0)
+      if((ret = recv(sock,&c,1,MSG_PEEK))> 0)
       {
          if(c == '\n') 
            recv(sock,&c,1,0);
@@ -57,26 +189,32 @@ int get_line(int sock,char buf[],int length)
     }
     buf[i++] = c;
   }
-  if(ret < 0)
+ if(ret <= 0)
   {
       return ret;
   }
+  buf[i] = 0;
   return i;
 }
-void deal_msg(int sock)
+int deal_msg(int sock)
 {
+  /*SetNoBlock(sock);*/
   char buf[BUFF_SIZE]; 
+#ifdef DEBUG
   while(1)
   {
     int ret = get_line(sock,buf,sizeof(buf));
     if( ret <= 0)
       break;
-    buf[ret] = 0;
     printf("%s",buf);
   }
-  exit(1);
+#endif
+ get_line(sock,buf,sizeof(buf));
+ Deal_Header(buf,sock);
+
+ exit(1);
 }
-void deal_sock(int sock, struct sockaddr_in* clinet_addr,int listen_sock)
+int deal_sock(int sock, struct sockaddr_in* clinet_addr,int listen_sock)
 {
   int pid  = fork();
   if(pid == 0)
@@ -97,7 +235,7 @@ void deal_sock(int sock, struct sockaddr_in* clinet_addr,int listen_sock)
 int main()
 {
   int listen_sock = startup();
-  printf("listen_sock : %d\n",listen_sock);
+  /*printf("listen_sock : %d\n",listen_sock);*/
     while(1)
     {
         struct sockaddr_in clinet_addr;
---
+++ OS/Internet/HTTP/service.h
@@ -0,0 +1,24 @@
+#pragma once
+#include<stdio.h>
+#include<stdlib.h>
+#include<sys/types.h>
+#include<sys/socket.h>
+#include<arpa/inet.h>
+#include<unistd.h>
+#include<sys/wait.h>
+#include<strings.h>
+#include <string.h>
+#include<ctype.h>
+
+#include<sys/sendfile.h>
+#include <sys/stat.h>
+#include<fcntl.h>
+
+#define BUFF_SIZE 1024 
+#define PORT 8080
+int startup();
+int  ClearRequest(int sock);
+int Deal_Header( char* buf,int sock);
+int  get_line(int sock,char buf[],int length);
+int deal_msg(int sock);
+int     deal_sock(int sock, struct sockaddr_in* clinet_addr,int listen_sock);
